27c6ecf4565b2ea36244abd7bcb662fab9b051bd
Masum Ahmed <masumahmed1798@gmail.com>
2021-06-11 00:58:53 -0400

    addR() and addC() implemented

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
+
+const tr = "<tr></tr>";
+const td = "<td></td>";
+const table = document.querySelector("tbody");
 
 //Adds a row
 function addR() {
-    alert("Clicked Add Row")
+    numRows++;
+    updateRowCounter()
+    table.innerHTML += tr;
+
+    for (let i=0; i<numCols; i++)
+        table.querySelectorAll("tr")[numRows - 1].innerHTML += td;
 }
 //Adds a column
 function addC() {
-    alert("Clicked Add Col")
+    numCols++;
+    updateColCounter();
+
+    for (let i = 0; i < numRows; i++) {
+        table.querySelectorAll("tr")[i].innerHTML += td;
+    }
 }
 
 //Removes a row
@@ -20,19 +34,27 @@ function removeC() {
     alert("Clicked Remove Col")
 }
 //sets global var for selected color
-function selected(){
+function selected() {
     colorSelected = document.getElementById("selectedID").value;
     console.log(colorSelected);
 }
 
-function fill(){
+function fill() {
     alert("Clicked Fill All")
 }
 
-function clearAll(){
+function clearAll() {
     alert("Clicked Clear All")
 }
 
-function fillU(){
+function fillU() {
     alert("Clicked Fill All Uncolored")
+}
+
+function updateRowCounter() {
+    document.querySelector("#row").innerHTML = "ROW: " + numRows;
+}
+
+function updateColCounter() {
+    document.querySelector("#col").innerHTML = "COL: " + numCols;
 }
